1fab71cecc4d1b3305ad24e0652339e248dacee5
9929eric <43610219+9929eric@users.noreply.github.com>
2018-10-01 22:13:24 -0400

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,18 @@
+angry = True
+bored = True
+hungry = False
+tired = False
+if(angry and hungry and bored):
+  print('T-Rex eats the Triceratops.')
+elif(hungry and tired):
+  print('T-Rex eats the Iguanadon.')
+elif(hungry and bored):
+  print('T-Rex eats the Stegasaurus.')
+elif tired:
+  print('T-Rex goes to sleep.')
+elif(angry and bored):
+  print('T-Rex fights with the Velociraptor.')
+elif(angry or bored):
+  print('T-Rex roars.')
+else:
+  print('T-Rex gives a toothy smile.');
